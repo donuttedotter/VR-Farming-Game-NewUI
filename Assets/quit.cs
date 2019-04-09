5619dfe89d09fd95fbda48755645d0094941db97
donuttedotter <hanvism@gmail.com>
2019-04-08 18:59:45 -0700

    UI updates

---
+++ Assets/quit.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class quit : MonoBehaviour
+{
+    public void quitApp()
+    {
+        Debug.Log("Quit successfully");
+        Application.Quit();
+    }
+}
